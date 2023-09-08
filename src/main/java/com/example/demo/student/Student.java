15c0b0480fcda815b0e7252d8a70e47a90d5278a
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2023-09-08 07:55:45 +0200

    more changes

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -22,6 +23,7 @@ public class Student {
     private String email;
     private LocalDate dob;
 
+    @Transient
     private Integer age;
 
 
@@ -29,17 +31,15 @@ public class Student {
     public Student() {
     }
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
-        this.age = age;
         this.dob = dob;
         this.email = email;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
-        this.age = age;
         this.dob = dob;
         this.email = email;
     }
@@ -53,7 +53,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,20 +1,69 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Mariam",
-                "mariam.jamal@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 5),
-                21));
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+       Optional<Student> studentByEmail =
+               studentRepository.findStudentByEmail(student.getEmail());
+
+       if (studentByEmail.isPresent()) {
+           throw new IllegalStateException("email taken");
+       }
+    studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() ->
+                new IllegalStateException("student with id " + studentId + " does not exist"));
+
+        if(name != null &&
+                !name.isEmpty() &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if(email != null &&
+                !email.isEmpty() &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if(studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+             student.setEmail(email);
+        }
     }
 }
